33dbb8bdc69cef3d810ad912d8f675e652992420
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-11-23 13:35:36 +0000

    fix: commit missing files

--- client/src/services/queryService/queryServiceInterfaces.ts
+++ client/src/services/queryService/queryServiceInterfaces.ts
@@ -2,6 +2,20 @@ export interface User {
   display_name: string;
   username: string;
   points: string;
+  listings?: Listing[];
   listingCount: number;
   feedback_score: string;
+}
+
+export interface Listing {
+  asin: string;
+  book: Book;
+  listed_on: string;
+  condition: string;
+}
+
+export interface Book {
+  title: string;
+  author: string;
+  cover_art_url: string;
 }
